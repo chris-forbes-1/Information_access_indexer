6efa61003cd20c748e3066b6df5f475e873628e0
xgb10162 <christopher.forbes@strath.ac.uk>
2012-10-12 13:50:14 +0100

    Initial Commit
    i think it works

---
+++ src/indexer/Parser.java
@@ -0,0 +1,22 @@
+package indexer;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.IndexWriter;
+public class Parser {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		 
+		
+
+	}
+
+}
